08ba36637992f329625cbd09e1a3fcc5ddba80b8
Leonard Richardson <leonardr@segfault.org>
2019-10-05 07:08:07 -0400

    Brought setup.py up to date with the bs4 version so I can put out what is hopefully the final release of Beautiful Soup 3.

--- setup.py
+++ setup.py
@@ -1,64 +1,36 @@
-from distutils.core import setup
-import unittest
-import warnings
-warnings.filterwarnings("ignore", "Unknown distribution option")
-
 # The very first thing we do is give a useful error if someone is
 # running this code under Python 3.
 "You're trying to run a very old release of Beautiful Soup under Python 3. This will not work."<>"Please use Beautiful Soup 4, available through the pip package 'beautifulsoup4'."
 
-import sys
-# patch distutils if it can't cope with the "classifiers" keyword
-if sys.version < '2.2.3':
-    from distutils.dist import DistributionMetadata
-    DistributionMetadata.classifiers = None
-    DistributionMetadata.download_url = None
-
+from setuptools import (
+    setup,
+    find_packages,
+)
 from BeautifulSoup import __version__
 
-#Make sure all the tests complete.
-import BeautifulSoupTests
-loader = unittest.TestLoader()
-result = unittest.TestResult()
-suite = loader.loadTestsFromModule(BeautifulSoupTests)
-suite.run(result)
-if not result.wasSuccessful():
-    print "Unit tests have failed!"
-    for l in result.errors, result.failures:
-        for case, error in l:
-            print "-" * 80
-            desc = case.shortDescription()
-            if desc:
-                print desc
-            print error        
-    print '''If you see an error like: "'ascii' codec can't encode character...", see\nthe Beautiful Soup documentation:\n http://www.crummy.com/software/BeautifulSoup/documentation.html#Why%20can't%20Beautiful%20Soup%20print%20out%20the%20non-ASCII%20characters%20I%20gave%20it?'''
-    print "This might or might not be a problem depending on what you plan to do with\nBeautiful Soup."
-    if sys.argv[1] == 'sdist':
-        print
-        print "I'm not going to make a source distribution since the tests don't pass."
-        sys.exit(1)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name="BeautifulSoup",
-      version=__version__,
-      py_modules=['BeautifulSoup', 'BeautifulSoupTests'],
-      description="HTML/XML parser for quick-turnaround applications like screen-scraping.",
-      author="Leonard Richardson",
-      author_email = "leonardr@segfault.org",
-      long_description="""Beautiful Soup parses arbitrarily invalid SGML and provides a variety of methods and Pythonic idioms for iterating and searching the parse tree.""",
-      classifiers=["Development Status :: 5 - Production/Stable",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: Python Software Foundation License",
-                   "Programming Language :: Python",
-                   "Topic :: Text Processing :: Markup :: HTML",
-                   "Topic :: Text Processing :: Markup :: XML",
-                   "Topic :: Text Processing :: Markup :: SGML",
-                   "Topic :: Software Development :: Libraries :: Python Modules",
-                   ],
-      url="http://www.crummy.com/software/BeautifulSoup/",
-      license="BSD",
-      download_url="http://www.crummy.com/software/BeautifulSoup/download/"
-      )
-    
-    # Send announce to:
-    #   python-announce@python.org
-    #   python-list@python.org
+setup(
+    name="BeautifulSoup",
+    version = __version__,
+    author="Leonard Richardson",
+    author_email='leonardr@segfault.org',
+    url="http://www.crummy.com/software/BeautifulSoup/",
+    download_url = "http://www.crummy.com/software/BeautifulSoup/download/",
+    description="Screen-scraping library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    py_modules=['BeautifulSoup', 'BeautifulSoupTests'],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: Python Software Foundation License",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Text Processing :: Markup :: HTML",
+                 "Topic :: Text Processing :: Markup :: XML",
+                 "Topic :: Text Processing :: Markup :: SGML",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+             ],
+)
